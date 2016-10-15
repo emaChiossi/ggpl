aea5ae5810b4737bec10e177200c41bd78233083
emaChiossi <ema.chiossi@gmail.com>
2016-10-15 11:25:05 +0200

    workshop1

--- 2016-10-14/workshop_01.py
+++ 2016-10-14/workshop_01.py
@@ -6,30 +6,32 @@ def create(beamSection, pillarSection,distanceBetweenPillar, interstoryHeight):
 	bz = beamSection[1]
 	px = pillarSection[0]
 	py = pillarSection[1]
-	pz = interstoryHeight[0]
-	Xpil = []
+	Xpil = []	
+	Xbea = [bx]
+	if bx>px:
+		Xpil = [-(DIV([bx,2])-DIV([px,2]))]
+	if bx<px:
+		Xbea = [-(DIV([px,2])-DIV([bx,2])),bx]	
 	Ypil = []
 	Zpil = []
-	Xbea = []
-	Ybea = [py]
+	Ybea = []
 	Zbea = []
 	lunghezza = 0
-	#costruzione pilastro
 	for i in distanceBetweenPillar:
-		Xpil = Xpil+[px,-i]
-		Ypil = [py]
-		lunghezza=lunghezza+i+px
+		Ypil = Ypil+[py,-i]
+		lunghezza=lunghezza+i+py
+	Xpil = Xpil+[px]
 	for i in interstoryHeight:
 		Zpil = Zpil+[i,-bz]
 		Zbea = Zbea+[-i,bz]
-	Xbea = [lunghezza+px] 
-	Xpil = Xpil+[px]	
+	Ybea = [lunghezza+py] 
+	Ypil = Ypil+[py]	
+	#costruzione pilastro
 	aPil = PROD([QUOTE(Ypil),QUOTE(Zpil)])
 	piloni = PROD([QUOTE(Xpil),aPil])
 	#costruzione trave
 	aTrav = PROD([QUOTE(Ybea),QUOTE(Zbea)])
 	travi=PROD([QUOTE(Xbea),aTrav])
-
 	VIEW(STRUCT([piloni,travi]))
 
 create((.4,.4),(.4,.4),[4,4,4],[3,3,3])
